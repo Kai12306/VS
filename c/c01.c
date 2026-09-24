23624d8ab111c3f3f1111c7f35037baac12adc68
Kai12306 <13713809305@163.com>
2026-09-25 00:35:21 +0800

    C语言冲刺 D1-D3: c01 第一个程序 / c02 sqrt-pow-fabs / c03 常量输出练习

---
+++ c/c01.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("³¬¼¶ÎÞµÐË§!");
+	return 0;
+} 
---
+++ c/c02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	double x = 2.0;
+	double result = sqrt(x);
+	printf("sqrt(2) = %f\n", result);
+	printf("2^10   =%f\n", pow(2.0, 10.0));
+	printf("abs(-5.5) = % f\n", fabs(-5.5));
+	return 0;
+}
